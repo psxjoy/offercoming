55e5fdc06bca917373edc5d934b5fe0211acce75
psxjoy <psxjoy@outlook.com>
2020-09-23 01:25:38 +0800

    leetcode add

---
+++ 算法/1.两数之和.java
@@ -0,0 +1,50 @@
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (48.38%)
+ * Likes:    8885
+ * Dislikes: 0
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int anotherNum = target-nums[i];
+            if(map.containsKey(anotherNum)) {
+                return new int[]{map.get(anotherNum),i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+// @lc code=end
+
